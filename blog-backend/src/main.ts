0048816875e98b041ebfda845c61f7eebcdf8fdc
waliqs <>
2025-08-01 16:01:31 +0500

    update

--- blog-backend/src/main.ts
+++ blog-backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  const port = parseInt(process.env.PORT || '5000', 10);
+  const port = parseInt(process.env.PORT || '5012', 10);
   await app.listen(port);
 }
 bootstrap();
